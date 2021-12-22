588bae977515334facb8427456aaef7e07ea0560
Sakhawat Hussain <fcssakhawat@gmail.com>
2021-12-22 22:59:36 +0000

    Inheritance in Java

---
+++ Inheritance/ExtraRandom.java
@@ -0,0 +1,16 @@
+package Inheritance;
+
+import java.util.Random;
+
+public class ExtraRandom extends Random {
+
+    public String nextLetter()
+    {
+        int aAsInt = (int)'a';
+        System.out.println(aAsInt);
+        return "";
+
+    }
+
+
+}
